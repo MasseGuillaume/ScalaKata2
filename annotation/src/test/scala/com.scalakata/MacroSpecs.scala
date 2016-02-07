eb5fb1f34cc45b4460ac4423234e60a5546b977d
Guillaume Massé <masgui@gmail.com>
2016-02-07 18:25:17 -0500

    implicitly[Ordering] make test fail with different jdk

--- annotation/src/test/scala/com.scalakata/MacroSpecs.scala
+++ annotation/src/test/scala/com.scalakata/MacroSpecs.scala
@@ -23,7 +23,6 @@ class MacroSpecs extends org.specs2.Specification { def is = s2"""
 @instrument class Full {
   val a: Option[Int] = Some(1)
   a match { case None => 1; case Some(v) => v}
-  implicitly[Ordering[Int]]
   1.0: Double
   for { v <- Some(1) } yield v
   identity(1)
@@ -33,15 +32,14 @@ class MacroSpecs extends org.specs2.Specification { def is = s2"""
   try { 1 / 0 } catch { case scala.util.control.NonFatal(e) => 0 }
 }
     withOffset(new Full) ==== List(
-      (RangePosition( 33, 33, 77), Value("1", "Int")), 
-      (RangePosition( 80, 80,105), Value("scala.math.Ordering$Int$@72b11c6d", "scala.math.Ordering[Int]")),
-      (RangePosition(108,108,119), Value("1.0", "Double")), 
-      (RangePosition(122,122,150), Value("Some(1)", "scala.Option[Int]")), 
-      (RangePosition(153,153,164), Value("1", "Int")), 
-      (RangePosition(167,167,177), Value("List()", "scala.collection.immutable.List[Nothing]")), 
-      (RangePosition(180,180,181), Value("Some(1)", "scala.Option[Int]")), 
-      (RangePosition(185,185,191), Value("Some(1)", "scala.Option[Int]")), 
-      (RangePosition(194,194,258), Value("0", "Int"))
+      (RangePosition(33,33,77),   Value("1","Int")),
+      (RangePosition(80,80,91),   Value("1.0","Double")),
+      (RangePosition(94,94,122),  Value("Some(1)","scala.Option[Int]")),
+      (RangePosition(125,125,136),Value("1","Int")),
+      (RangePosition(139,139,149),Value("List()","scala.collection.immutable.List[Nothing]")),
+      (RangePosition(152,152,153),Value("Some(1)","scala.Option[Int]")),
+      (RangePosition(157,157,163),Value("Some(1)","scala.Option[Int]")),
+      (RangePosition(166,166,230),Value("0","Int"))
     )
   }
 }

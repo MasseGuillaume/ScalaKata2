7a7c3263e8f3feee1ad08436aaa391229f781c6c
Guillaume Massé <masgui@gmail.com>
2016-02-14 16:50:38 -0500

    fix #8 and fix #7

--- annotation/src/main/scala/com.scalakata/Macro.scala
+++ annotation/src/main/scala/com.scalakata/Macro.scala
@@ -11,52 +11,99 @@ object KataMacro {
         q"_root_.com.scalakata.RangePosition(${p.start}, ${p.start}, ${p.end})"
       }
 
-      def w(aTree: Tree) = {
+      def w(aTree: Tree, tTree: Option[Tree] = None) = {
         val t = TermName(c.freshName)
         if(aTree.pos == NoPosition) aTree
         else {
-          q"""{
-            val $t = $aTree
-            ${instrumentation}(${aTree.pos}) = render($t)
-            $t
-          }"""
+          tTree match {
+            case None =>
+              q"""{
+                val $t = $aTree
+                ${instrumentation}(${aTree.pos}) = render($t)
+                $t
+              }"""
+            case Some(tpe) =>
+              q"""{
+                val $t: $tpe = $aTree
+                ${instrumentation}(${aTree.pos}) = render($t)
+                $t
+              }"""
+          }
         }
+      }
+
+      def extract(vd: ValDef) = {
+        val ValDef(mods, name, tpt, expr) = vd
+
+        // val (a, b) = (1, 2) would add a SYNTHETIC tree, we dont instrument it
+        if(mods.hasFlag(Flag.SYNTHETIC)) vd
+        else ValDef(mods, name, tpt, w(expr, Some(tpt)))
       }
 
       // see http://docs.scala-lang.org/overviews/quasiquotes/syntax-summary.html
       tree match {
-        case q"$_ match { case ..$_ }" ⇒ w(tree)
-        case q"$expr[..$tpts]"         ⇒ w(tree)   // implicitly[Ordering[Int]]
-        case q"$expr: $tpt"            ⇒ w(tree)   // a: Int
-        case q"for (..$enums) $expr"   ⇒ tree
-        case _: Apply                  ⇒ w(tree)   // f(1)
-        case _: Select                 ⇒ w(tree)   // p.x
-        case _: Ident                  ⇒ w(tree)   // p
-        case _: Block                  ⇒ w(tree)   // {a; b}
-        case _: Try                    ⇒ w(tree)   // try ...
-        case v                         ⇒ v
+        case q"println {..$body}"              ⇒ w(q"{..$body}")
+        case q"$expr(..$exprs) = $rhs"         ⇒ q"$expr(..$exprs) = ${w(rhs)}"
+        case q"if ($cond) $texpr else $fexpr"  ⇒ q"if ($cond) ${w(texpr)} else ${w(fexpr)}"
+        case q"for (..$enums) yield $expr"     ⇒ w(tree)
+        case q"while ($_) $_"                  ⇒ tree
+        case q"$expr1 = $expr2"                ⇒ q"$expr1 = ${w(expr2)}"
+        case q"$_ match { case ..$_ }"         ⇒ w(tree)
+        case q"$expr[..$tpts]"                 ⇒ w(tree)                                      // implicitly[Ordering[Int]]
+        case q"$expr: $tpt"                    ⇒ w(tree, Some(tpt))                           // a: Int
+        case q"$expr match { case ..$cases }"  ⇒ w(tree)
+        case q"while ($cond) $expr"            ⇒ tree
+        case q"do $cond while ($expr)"         ⇒ tree
+        case q"for (..$enums) $expr"           ⇒ tree
+        case vd: ValDef                        ⇒ extract(vd)                                  // var / val
+        case _: Apply                          ⇒ w(tree)                                      // f(1)
+        case _: Select                         ⇒ w(tree)                                      // p.x
+        case _: Ident                          ⇒ w(tree)                                      // p
+        case b: Block                          ⇒ w(tree)                                      // {a; b}
+        case _: Try                            ⇒ w(tree)                                      // try ...
+        case lit: Literal                      ⇒ lit                                          // 1.0
+        case ld: LabelDef                      ⇒ ld                                           // do / while
+        case cd: ClassDef                      ⇒ cd                                           // class A / trait A
+        case md: ModuleDef                     ⇒ md                                           // object A
+        case td: TypeDef                       ⇒ td                                           // type A = List
+        case dd: DefDef                        ⇒ dd                                           // def f = 1
+        case im: Import                        ⇒ im
+        case v                                 ⇒ { println(showRaw(v)); v }
       }
     }
 
+    
     c.Expr[Instrumented]{
       annottees.map(_.tree).toList match { case q"class $name { ..$body }" :: Nil ⇒
         val instrumentation = TermName(c.freshName)
 
         val offset = 
           c.enclosingPosition.end + (
-            " " +
-            s"""|class $name {
-                |""".stripMargin
-          ).length
+          " " +
+          s"""|class $name {
+              |""".stripMargin).length
 
-        q"""
-        class $name extends Instrumented {
-          private val $instrumentation = scala.collection.mutable.Map[_root_.com.scalakata.RangePosition, Render]()
-          def offset$$ = $offset
-          def instrumentation$$: _root_.com.scalakata.Instrumentation = ${instrumentation}.toList.sorted
-          ..${body.map(t ⇒ instrumentOne(t, instrumentation))}
+        try {
+          q"""
+          class $name extends Instrumented {
+            private val $instrumentation = scala.collection.mutable.Map[_root_.com.scalakata.RangePosition, Render]()
+            def offset$$ = $offset
+            def instrumentation$$: _root_.com.scalakata.Instrumentation = ${instrumentation}.toList.sorted
+            ..${body.map(t ⇒ instrumentOne(t, instrumentation))}
+          }
+          """
+        } catch {
+          case scala.util.control.NonFatal(e) => {
+            println(s"compiler bug ${e.toString}")
+            q"""
+            class $name extends Instrumented {
+              private val $instrumentation = scala.collection.mutable.Map[_root_.com.scalakata.RangePosition, Render]()
+              def offset$$ = $offset
+              def instrumentation$$: _root_.com.scalakata.Instrumentation = ${instrumentation}.toList.sorted
+            }
+            """
+          }
         }
-        """
       }
     }
   }
--- annotation/src/test/scala/com.scalakata/MacroSpecs.scala
+++ annotation/src/test/scala/com.scalakata/MacroSpecs.scala
@@ -1,45 +1,109 @@
 package com.scalakata
 
-import scala.collection.mutable.{Map ⇒ MMap}
-
 class MacroSpecs extends org.specs2.Specification { def is = s2"""
   Kata Macro Specifications
+    var/val $varVal
     full $full
+    val extraction $extraction
+    typesplit $typesplit
 """
 
   def withOffset(instr: Instrumented) = {
     val by = instr.offset$
-    instr.instrumentation$.map{ case
-     
-     (RangePosition(start, pos, end), repr) ⇒
-     
-      
+    instr.instrumentation$.map{ case (RangePosition(start, pos, end), repr) ⇒
       (RangePosition(start - by, pos - by, end - by), repr)
     }
   }
 
-  def full = {
-
-@instrument class Full {
-  val a: Option[Int] = Some(1)
-  a match { case None => 1; case Some(v) => v}
-  1.0: Double
-  for { v <- Some(1) } yield v
-  identity(1)
-  List.empty
-  a;
-  {a; a}
-  try { 1 / 0 } catch { case scala.util.control.NonFatal(e) => 0 }
+  def varVal = {
+@instrument class VarVal {
+val a = 1 + 1
+var b = 2 + 2
 }
-    withOffset(new Full) ==== List(
-      (RangePosition(33,33,77),   Value("1","Int")),
-      (RangePosition(80,80,91),   Value("1.0","Double")),
-      (RangePosition(94,94,122),  Value("Some(1)","scala.Option[Int]")),
-      (RangePosition(125,125,136),Value("1","Int")),
-      (RangePosition(139,139,149),Value("List()","scala.collection.immutable.List[Nothing]")),
-      (RangePosition(152,152,153),Value("Some(1)","scala.Option[Int]")),
-      (RangePosition(157,157,163),Value("Some(1)","scala.Option[Int]")),
-      (RangePosition(166,166,230),Value("0","Int"))
+
+    withOffset(new VarVal) ====  List(
+      RangePosition( 8,  8, 13) -> Value("2", "Int"),
+      RangePosition(22, 22, 27) -> Value("4", "Int")
     )
+  }
+
+  def full = {
+@instrument class Full {
+val a = "L29"
+var b = "L30"
+a + b
+println(a + b)
+Set(33)
+case class L34(b: Int){ def v = "L34v" }
+class L35
+trait L36
+object L37
+type L38[V] = List[V]
+L34(39).v
+def f = "L40"
+f
+("L42-1", "L42-2") match { case (a, b) ⇒ a + b }
+//
+implicitly[Ordering[Int]].lt(1, 2)
+var d = "L45"
+d = "L46"
+val m = collection.mutable.Map(1 -> 47)
+m(1) = 48
+
+{
+  val a = "53"
+  val b = "54"
+  a + b
+  a + b + b
+}
+while(d == "L46") {
+  d = "L56"
+}
+
+if(true) "L56-t" else "L56-f"
+if(true) null
+}
+
+    withOffset(new Full) ==== List(
+      RangePosition(  8,   8,  13) -> Value("L29", "java.lang.String"),
+      RangePosition( 22,  22,  27) -> Value("L30", "java.lang.String"),
+      RangePosition( 28,  28,  33) -> Value("L29L30", "java.lang.String"),
+      RangePosition( 42,  42,  47) -> Value("L29L30", "java.lang.String"),
+      RangePosition( 49,  49,  56) -> Value("Set(33)", "scala.collection.immutable.Set[Int]"),
+      RangePosition(151, 151, 160) -> Value("L34v", "java.lang.String"),
+      RangePosition(175, 175, 176) -> Value("L40", "java.lang.String"),
+      RangePosition(177, 177, 225) -> Value("L42-1L42-2", "java.lang.String"),
+      RangePosition(229, 229, 263) -> Value("true", "Boolean"),
+      RangePosition(272, 272, 277) -> Value("L45", "java.lang.String"),
+      RangePosition(282, 282, 287) -> Value("L46", "java.lang.String"),
+      RangePosition(296, 296, 327) -> Value("Map(1 -> 47)", "scala.collection.mutable.Map[Int, Int]"),
+      RangePosition(335, 335, 337) -> Value("48", "Int"),
+      RangePosition(339, 339, 392) -> Value("535454", "java.lang.String"),
+      RangePosition(437, 437, 444) -> Value("L56-t", "java.lang.String"),
+      RangePosition(467, 467, 471) -> Value("null", "Null")
+    )
+  }
+
+  def extraction = {
+@instrument class Extraction {
+  val (a, b) = (1, 2)
+}
+
+    withOffset(new Extraction) ====  List(
+      (RangePosition(7,7,7), Value("1", "Int")),
+      (RangePosition(10,10,10), Value("2", "Int"))
+    )
+  }
+
+  def typesplit = {
+@instrument class TypeSplit {
+  val withDefault: Option[Int] => Int = {
+    case Some(x) => x
+    case None => 0
+  }
+}
+
+    withOffset(new TypeSplit) ====
+      List((RangePosition(40,40,86),Value("<function1>","scala.Function1[scala.Option[Int], Int]")))
   }
 }
